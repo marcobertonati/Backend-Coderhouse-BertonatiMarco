7bbe9b770ae0e5bb3c44f8a83967a2fb8875d11c
Marco Bertonati <marko.bertonati@gmail.com>
2021-06-21 18:01:57 -0300

    =Desafio 3

---
+++ Desafio3/JABertonatiMarco.js
@@ -0,0 +1,30 @@
+function showParr(texto, quantyWordText, totalWords, timeInterval) {
+    console.log('Entro a la función ShowParr')
+
+    let arrayText = texto.split(" ");
+    let indice = 0;
+    let myInterval = setInterval(()=> {
+
+        console.log(`👉 ${arrayText[indice]}`)
+        indice++;
+
+        if(indice==arrayText.length) {
+            clearInterval(myInterval);
+            console.log('setInterval finalizado')
+            console.log('/*----------------*/');
+            totalWords(quantyWordText+arrayText.length);
+        }
+
+    }, timeInterval)
+
+    console.log('Salio de la función ShowParr')
+}
+
+showParr("Texto Uno", 0, totalWords => {
+    showParr('Parrafo dos', totalWords, totalWords => {
+        showParr('Conclusión 3', totalWords, totalWords => {
+            console.log(`Proceso terminado.`)
+            console.log(`La cantidad de palabras total es ${totalWords}.`)
+        }, 2000)
+    }, 2000 )
+},2000);
