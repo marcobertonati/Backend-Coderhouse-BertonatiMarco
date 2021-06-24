0a2e2201c5f72b4b08078a5eeda397b65971cbab
Marco Bertonati <marko.bertonati@gmail.com>
2021-06-23 23:51:43 -0300

    =Desafio 3 modificado, y desafio 4 listo

--- Desafio3/JABertonatiMarco.js
+++ Desafio3/JABertonatiMarco.js
@@ -1,30 +1,43 @@
 function showParr(texto, quantyWordText, totalWords, timeInterval) {
-    console.log('Entro a la función ShowParr')
+  console.log("Entro a la función ShowParr");
 
-    let arrayText = texto.split(" ");
-    let indice = 0;
-    let myInterval = setInterval(()=> {
+  let arrayText = texto.split(" ");
+  let indice = 0;
+  let myInterval = setInterval(() => {
+    console.log(`👉 ${arrayText[indice]}`);
+    indice++;
 
-        console.log(`👉 ${arrayText[indice]}`)
-        indice++;
+    if (indice == arrayText.length) {
+      clearInterval(myInterval);
+      console.log("setInterval finalizado");
+      console.log("/*----------------*/");
+      totalWords(quantyWordText + arrayText.length);
+    }
+  }, timeInterval);
 
-        if(indice==arrayText.length) {
-            clearInterval(myInterval);
-            console.log('setInterval finalizado')
-            console.log('/*----------------*/');
-            totalWords(quantyWordText+arrayText.length);
-        }
-
-    }, timeInterval)
-
-    console.log('Salio de la función ShowParr')
+  console.log("Salio de la función ShowParr");
 }
 
-showParr("Texto Uno", 0, totalWords => {
-    showParr('Parrafo dos', totalWords, totalWords => {
-        showParr('Conclusión 3', totalWords, totalWords => {
-            console.log(`Proceso terminado.`)
-            console.log(`La cantidad de palabras total es ${totalWords}.`)
-        }, 2000)
-    }, 2000 )
-},2000);
+showParr(
+  "Texto Uno",
+  0,
+  (totalWords) => {
+    showParr(
+      "Parrafo dos",
+      totalWords,
+      (totalWords) => {
+        showParr(
+          "Conclusión 3",
+          totalWords,
+          (totalWords) => {
+            console.log(`Proceso terminado.`);
+            console.log(`La cantidad de palabras total es ${totalWords}.`);
+          },
+          2000
+        );
+      },
+      2000
+    );
+  },
+  2000
+);
