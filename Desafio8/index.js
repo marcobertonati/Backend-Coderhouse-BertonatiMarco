b1669afbc19df23cbabf1da276be72e29edb6c48
Marco Bertonati <marko.bertonati@gmail.com>
2021-07-12 23:07:01 -0300

    =Desafio 8

--- Desafio8/index.js
+++ Desafio8/index.js
@@ -18,10 +18,10 @@ let listProducts = [];
 /*Rutas*/
 app.get("/api/productos/listar", (req, res) => {
   if (listProducts.length <= 0) {
-    console.log('No se encontraron productos')
+    console.log("No se encontraron productos");
     res.send({ error: "No hay productos cargados" });
   } else {
-    console.log('Se encontraron productos')
+    console.log("Se encontraron productos");
     res.send(listProducts);
   }
 });
@@ -32,8 +32,8 @@ app.get("/api/productos/listar/:id", (req, res) => {
   );
 
   if (productCheck) {
-    console.log(listProducts[req.params.id]);
-    res.send(listProducts[req.params.id]);
+    console.log(listProducts[req.params.id - 1]);
+    res.send(listProducts[req.params.id - 1]);
   } else {
     console.log("Producto no encontrado");
     res.send({ error: "producto no encontrado" });
@@ -42,14 +42,35 @@ app.get("/api/productos/listar/:id", (req, res) => {
 
 app.post("/api/productos/guardar", (req, res) => {
   let newProductBody = req.body;
-  let newProduct = new Product(
-    newProductBody.title,
-    newProductBody.price,
-    newProductBody.thumbnail
-  );
-  listProducts.push(newProduct);
-  res.send(newProduct);
+
+  if (listProducts.length == 0) {
+    let newProduct = new Product(
+      newProductBody.title,
+      newProductBody.price,
+      newProductBody.thumbnail,
+      listProducts.length + 1
+    );
+
+    listProducts.push(newProduct);
+    res.send(newProduct);
+
+  } else {
+    let newProduct = new Product(
+      newProductBody.title,
+      newProductBody.price,
+      newProductBody.thumbnail,
+      listProducts[listProducts.length-1].id +1
+    );
+
+    listProducts.push(newProduct);
+    res.send(newProduct);
+  }
 });
+
+app.delete('/api/productos/borrar/:id', (req, res) => {
+    listProducts = listProducts.filter(product => product.id != req.params.id);
+    res.send(`Se borró el producto`)
+})
 
 /*Ejecución de servidor*/
 const PORT = 8080;
--- Desafio8/products.js
+++ Desafio8/products.js
@@ -1,11 +1,10 @@
 class Product {
 
-    id = 0
-    constructor(title, price, thumbnail, id = this.id) {
+    constructor(title, price, thumbnail, id) {
         this.title = title, 
         this.price = price,
         this.thumbnail = thumbnail,
-        this.id = ++id
+        this.id = id
     }
 
 }
